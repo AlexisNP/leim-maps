be9b2d5a7f1ea7f13e7b2bbe0ebef5f91112592b
Alexis <35.alexis.pele@gmail.com>
2024-01-16 00:12:07 +0100

    Added sitemap generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import vue from "@astrojs/vue";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [vue()],
-    output: 'static',
-    site: 'https://maps.alexcreates.fr',
+  integrations: [vue(), sitemap()],
+  output: 'static',
+  site: 'https://maps.alexcreates.fr'
 });
