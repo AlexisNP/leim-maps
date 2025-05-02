c282c7ec7ae45164a6b6accd6813b7988e2fdacd
Alexis <35.alexis.pele@gmail.com>
2025-05-02 18:54:28 +0200

    Added env variables to disable some options

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly ENABLE_TOOLBAR: boolean
+    readonly ENABLE_TOOLBAR_TAGS: boolean
+    readonly ENABLE_CUSTOM_MARKERS: boolean
+    readonly ENABLE_BREADCRUMB: boolean
+    readonly ENABLE_I18N: boolean
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
